f82db32498adb59fea64fd423bce430645957790
Jose Sainz <joedanielsainz@gmail.com>
2025-11-10 16:44:03 -0700

    fixed linkedin url

--- app/page.tsx
+++ app/page.tsx
@@ -57,7 +57,7 @@ export default function Home() {
             </a>
             <span className="text-gray-400">•</span>
             <a 
-              href="www.linkedin.com/in/sainz-gonzalez-daniel" 
+              href="https://www.linkedin.com/in/sainz-gonzalez-daniel"
               target="_blank" 
               rel="noopener noreferrer"
               className="text-gray-700 hover:text-black transition-colors"
